a43f87e2e5d9154bfa2bea587eee93c10b71e848
mamotromico <mamotromico@gmail.com>
2017-11-16 02:06:32 -0300

    Novo push

---
+++ scenes/Game.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	get_node("Box/dialog").show_text("Dia 1 - Chegada", "Intro")
